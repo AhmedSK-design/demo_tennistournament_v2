28be8ee66ece6e76f86b49c86fc9be2004c34b78
ahmedkarakaya <ahmed.karakaya@tum.de>
2025-10-31 09:13:11 +0100

    GUI functionalities like min. 4 Players

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [numPlayers, setNumPlayers] = useState(0)
   const [players, setPlayers] = useState<{ id: number; name: string }[]>([])
   const [showSetup, setShowSetup] = useState(true);
@@ -17,6 +16,7 @@ function App() {
   };
 
   const handleAddPlayers = () => {
+    if (numPlayers > 3){
     const newPlayers = Array.from({ length: numPlayers }, (_, i) => ({
       id: i,
       name: "",
@@ -25,16 +25,20 @@ function App() {
     setShowSetup(false);
     setShowback(true);
   }
+  }
 
   return (
     <>
     <div className='header'>
       <h1>Tennis Tournament</h1>
     </div>
+
+
       {showSetup &&(
         <div className="card">
-        <input type="text" placeholder="How many players" onChange={(e) => setNumPlayers(parseInt(e.target.value) || 0)} />
-          
+        <h2>Create a new Tournament</h2>
+        <input type="text" placeholder="How many players" onChange={(e) => 
+          setNumPlayers(parseInt(e.target.value) || 0)} />          
       <div className='button'>
         <button onClick={handleAddPlayers}>Add Player</button>
       </div>
@@ -43,12 +47,14 @@ function App() {
 
       {showback && (
         <div className='Players'>
+          <h3>Player information</h3>
         {players.map((player, i) => (
           <input
             key={player.id}
             type="text"
             placeholder={`Enter player ${i + 1} name`}
           />
+
         ))}
         <button onClick={backbutton}>Back</button>
       </div>
