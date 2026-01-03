673015d9695550c33474d5f6f9b09355821c4f6f
ahmedkarakaya <ahmed.karakaya@tum.de>
2026-01-03 15:08:22 +0100

    krise

---
+++ src/main/java/com/example/roundrobintunier/RoundrobintunierApplication.java
@@ -0,0 +1,11 @@
+package com.example.roundrobintunier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoundrobintunierApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RoundrobintunierApplication.class, args);
+    }
+}
